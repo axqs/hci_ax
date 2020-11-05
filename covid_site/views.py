1c17f3e98741b121e6f7642be4119fa9b2442afe
Q Smart <qiufei@me.com>
2020-11-05 19:25:37 +0000

    add gitignore, add basic example charts to main page

--- covid_site/views.py
+++ covid_site/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+#chart functions
+from graphos.sources.simple import SimpleDataSource
+from graphos.renderers.gchart import BarChart
+from graphos.renderers.gchart import LineChart
+from graphos.renderers.gchart import PieChart
+
 
 # Create your views here.
+def populate():
+    print("refresh charts")
+    charts = []
+    charts.append(LineChart(SimpleDataSource(data=[
+        ['Year', 'Sales', 'Expenses'],
+        [2004, 1000, 400],
+        [2005, 1170, 460],
+        [2006, 660, 1120],
+        [2007, 1030, 540]
+        ]
+    )))
+    charts.append(BarChart(SimpleDataSource(data=[
+        ['Year', 'Sales', 'Expenses'],
+        [2004, 1000, 400],
+        [2005, 1170, 460],
+        [2006, 660, 1120],
+        [2007, 1030, 540]
+        ]
+    )))
+    charts.append(PieChart(SimpleDataSource(data=[
+        ['Year', 'Sales'],
+        ["2004", 1000],
+        ["2005", 1170],
+        ["2006", 660],
+        ["2007", 1030]
+        ]
+    )))
+
+    return charts
+
+def index(request):
+    charts = populate()
+    context = {}
+
+    if len(charts) > 0:
+        context["charts"] = charts
+    
+    return render(request, 'covid_site/index.html', context)
--- hci_ax/settings.py
+++ hci_ax/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'covid_site',
+    'graphos'
 ]
 
 MIDDLEWARE = [
