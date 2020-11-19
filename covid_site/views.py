b8ac28a7b4c4b38ed88bcfbd7e28c211be5540b7
Q Smart <qiufei@me.com>
2020-11-19 17:24:22 +0000

    fill in rest of graphs with live data

--- covid_site/views.py
+++ covid_site/views.py
@@ -8,18 +8,24 @@ from datetime import date, datetime, timedelta
 
 # Create your views here.
 current_country = ''
-today = str(date.today())
+compare = ["","",""]
+months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
 yesterday = str(date.today() - timedelta(days=1))
 
-def getData():
+def getRawData():
     d = pd.read_csv('https://covid19.who.int/WHO-COVID-19-global-data.csv', sep=',', skipinitialspace=True)
     d.replace(to_replace=['United States of America', 'Russian Federation', 'The United Kingdom','Iran (Islamic Republic of)','Syrian Arab Republic','Viet Nam','Venezuela (Bolivarian Republic of)','Bolivia (Plurinational State of)','United Republic of Tanzania',],
            value= ['United States', 'Russia', 'United Kingdom','Iran','Syria','Vietnam','Venezuela','Bolivia','Tanzania',], 
            inplace=True)
-    if d[d['Date_reported']==today].empty:
-        return d[d['Date_reported']==yesterday]
-    return d[d['Date_reported']==today]
+    return d
 
+def getData():
+    d = getRawData()
+    return d[d['Date_reported']==yesterday]
+
+
+raw_data = getRawData()
 data = getData()
 
 def getCountries(): 
@@ -32,7 +38,6 @@ def getWorldData():
 
     world_data = world_data.values.tolist()
     world_data.insert(0,["Country","Total Deaths"])
-
     
     return world_data
 
@@ -44,8 +49,49 @@ def getCountryData():
     return {"cases":cData.iloc[0]['New_cases'],
             "deaths":cData.iloc[0]['New_deaths']}
 
+def getCompareData():
+    
+
+    with open("./covid_site/static/covid_site/compare1.txt","r") as f:
+        compare[0] = f.readlines()[0]
+    with open("./covid_site/static/covid_site/compare2.txt","r") as f:
+        compare[1] = f.readlines()[0]
+    with open("./covid_site/static/covid_site/compare3.txt","r") as f:
+        compare[2] = f.readlines()[0]
+
+    cData1 = raw_data[raw_data['Country']==compare[0]]
+    cData2 = raw_data[raw_data['Country']==compare[1]]
+    cData3 = raw_data[raw_data['Country']==compare[2]]
+
+    cData1 = cData1[["Date_reported","Country","New_cases","Cumulative_cases"]]
+    cData2 = cData2[["Date_reported","Country","New_cases","Cumulative_cases"]]
+    cData3 = cData3[["Date_reported","Country","New_cases","Cumulative_cases"]]
+
+    return [cData1,cData2,cData3]
+
 def populateCharts():
     charts = []
+    with open("./covid_site/static/covid_site/country.txt","r") as f:
+        current_country = f.readlines()[0]
+    compareData = raw_data[raw_data['Country']==current_country]
+    cases = compareData[compareData.Date_reported.str.contains('01$')]
+
+    cases = list(cases.index.values)
+    cases = [c-1 for c in cases]
+    cases = [raw_data.iloc[month][["Cumulative_cases"]].item() for month in cases]
+    cases.append(compareData.tail(1)["Cumulative_cases"].item())
+
+    deaths = compareData[compareData.Date_reported.str.contains('01$')]
+
+    deaths = list(deaths.index.values)
+    deaths = [c-1 for c in deaths]
+    deaths = [raw_data.iloc[month][["Cumulative_deaths"]].item() for month in deaths]
+    deaths.append((compareData.tail(1)["Cumulative_deaths"].item()))
+
+    timeseries = [['Month', 'Cases', 'Deaths']]
+    for i in range(int(datetime.now().strftime("%m"))):
+        timeseries.append(["End of "+months[i],cases[i],deaths[i]])
+
     charts.append({
         "size" : "wholeSpan",
         "id" : "worldwide",
@@ -56,88 +102,125 @@ def populateCharts():
     charts.append({
         "size" : "wholeSpan",
         "id" : "chart1",
-        "title" : current_country+" Closed Cases",
+        "title" : " Cumulative Cases and Deaths over Time",
         "type" : "line",
-        "data" : [
-            ['Month', 'Recoveries', 'Deaths'],
-            ["Mar", 1090, 520],
-            ["Apr", 700, 400],
-            ["May", 1170, 460],
-            ["Jun", 660, 1120],
-            ["Jul", 1030, 540],
-            ["Aug", 1000, 400],
-            ["Sep", 1170, 460],
-            ["Oct", 660, 1120],
-            ["Nov", 1030, 540],
-        ],
+        "data" : timeseries,
     })
     charts.append({
         "size" : "halfSpan",
         "id" : "chart3",
-        "title" : "Total Deaths per Age Range",
+        "title" : "Ratio of Cumulative Cases to Worldwide Cumulative Cases",
         "type" : "pie",
         "data" : [
-            ['Age Range', 'Deaths'],
-            ["0-18", 1000],
-            ["19-30", 100],
-            ["31-45", 1170],
-            ["46-65", 660],
-            ["66+", 1030]
+            ['Title', 'Number'],
+            [current_country, compareData.tail(1)["Cumulative_cases"].item()],
+            ["World", raw_data['New_cases'].sum()-compareData.tail(1)["Cumulative_cases"].item()],
+
         ],
     })
     return charts
 
 def populateComparisons():
     comparisons = []
+    compareData = getCompareData()
+    new_cases = []
+    for c in compareData:
+        new_cases.append(c.tail(1)["New_cases"].item())
+
+    monthData = []
+
+    for c in compareData:
+        m = c[c.Date_reported.str.contains('01$')]
+        m = list(m.index.values)
+        m = [m-1 for m in m]
+        m = [raw_data.iloc[month][["Cumulative_cases"]].item() for month in m]
+        monthData.append(m)
+    
+    cumulative_cases = []
+
+    for i in range(int(datetime.now().strftime("%m"))-1):
+        cumulative_cases.append([months[i],monthData[0][i],monthData[1][i],monthData[2][i]])
+    cumulative_cases.insert(0,["Month",compare[0],compare[1],compare[2]])
+
+    thisMonthData = [months[int(datetime.now().strftime("%m"))-1]]
+    for c in compareData:
+        thisMonthData.append(c.tail(1)["Cumulative_cases"].item())
+    cumulative_cases.append(thisMonthData)
+    
     comparisons.append({
         "size" : "halfSpan",
         "id" : "chart4",
-        "title" : " New Cases Today",
-        "type" : "line",
-        "data" : [
-            ['Month', 'Recoveries', 'Deaths'],
-            ["Mar", 1090, 520],
-            ["Apr", 700, 400],
-            ["May", 1170, 460],
-            ["Jun", 660, 1120],
-            ["Jul", 1030, 540],
-            ["Aug", 1000, 400],
-            ["Sep", 1170, 460],
-            ["Oct", 660, 1120],
-            ["Nov", 1030, 540],
-        ],
+        "title" : " Cumulative Cases as of "+(date.today() - timedelta(days=1)).strftime("%d %B, %Y"),
+        "type" : "area",
+        "data" : cumulative_cases,
     })
     comparisons.append({
         "size" : "halfSpan",
         "id" : "chart5",
-        "title" : " Cumulative Cases Today",
+        "title" : " New Cases as of "+ (date.today() - timedelta(days=1)).strftime("%d %B, %Y"),
         "type" : "bar",
         "data" : [
-            ['Element', 'Density'],
-            ['Copper', 8.94],           
-            ['Silver', 10.49],            
-            ['Gold', 19.30],
-            ['Platinum', 21.45],
+            ['Country', 'New Cases'],
+            [compare[0], new_cases[0]],           
+            [compare[1], new_cases[1]],            
+            [compare[2], new_cases[2]],
         ],
     })
+    return comparisons
 
 def index(request):
     if request.method == 'POST':
         current_country = request.POST.get('country')
-        with open("./covid_site/static/covid_site/country.txt","w") as f:
-            f.write(current_country)
+        compare[0] = request.POST.get('country1')
+        compare[1] = request.POST.get('country2')
+        compare[2] = request.POST.get('country3')
+
+        if current_country != None:
+            with open("./covid_site/static/covid_site/country.txt","w") as f:
+                f.write(current_country)
+        else:
+            with open("./covid_site/static/covid_site/country.txt","r") as f:
+                current_country = f.readlines()[0]
+
+        if compare[0] != None:
+            with open("./covid_site/static/covid_site/compare1.txt","w") as f:
+                f.write(compare[0])
+        else:
+            with open("./covid_site/static/covid_site/compare1.txt","r") as f:
+                compare[0] = f.readlines()[0]
+
+        if compare[1] != None:
+            with open("./covid_site/static/covid_site/compare2.txt","w") as f:
+                f.write(compare[1])
+        else:
+            with open("./covid_site/static/covid_site/compare2.txt","r") as f:
+                compare[1] = f.readlines()[0]
+
+        if compare[2] != None:
+            with open("./covid_site/static/covid_site/compare3.txt","w") as f:
+                f.write(compare[2])
+        else:
+            with open("./covid_site/static/covid_site/compare3.txt","r") as f:
+                compare[2] = f.readlines()[0]
     else:
         with open("./covid_site/static/covid_site/country.txt","r") as f:
             current_country = f.readlines()[0]
+        with open("./covid_site/static/covid_site/compare1.txt","r") as f:
+            compare[0] = f.readlines()[0]
+        with open("./covid_site/static/covid_site/compare2.txt","r") as f:
+            compare[1] = f.readlines()[0]
+        with open("./covid_site/static/covid_site/compare3.txt","r") as f:
+            compare[2] = f.readlines()[0]
 
     charts = populateCharts()
     comparisons = populateComparisons()
     context = {}
     context["countries"] = sorted(countries)
-    context["date"] = datetime.now().strftime("%d %B, %Y")
+    context["date"] = (date.today() - timedelta(days=1)).strftime("%d %B, %Y")
     context["current"] = current_country
     context["country_data"] = getCountryData()
     context["charts"] = charts
     context["comparisons"] = comparisons
+    context["compareCountries"] = compare
         
     return render(request, 'covid_site/index.html', context)
